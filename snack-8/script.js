2c34d9df9b228de09e6585e978f1f85834c6ff27
fabioadelfio <adelfiofabio07@gmail.com>
2025-04-22 14:59:42 +0200

    snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const searchStudent = students.find(student => student.name === `Marco Lanci`);
+
+console.log(searchStudent.class);
